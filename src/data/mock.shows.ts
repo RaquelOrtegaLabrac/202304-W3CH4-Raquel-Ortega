6a05fe852897a479cafc24663905020038182ca1
labrac <raquelortegalabrac@hotmail.com>
2023-05-18 20:04:46 +0200

    Update mock.shows.ts

--- src/data/mock.shows.ts
+++ src/data/mock.shows.ts
@@ -1,57 +1,44 @@
-import { type Show } from "../models/show";
+import { Show } from '../models/show';
 
 export const getMockShows = (): Show[] => [
-  {
-    id: 1,
-    name: "The Sopranos",
-    creator: "David Chase",
-    year: 1999,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
-    watched: false,
-    score: 0,
-    emmies: 21,
-  },
-  {
-    id: 2,
-    name: "Game of Thrones",
-    creator: "David Benioff D. B. Weiss",
-    year: 2011,
-    poster:
-      "https://t1.pixers.pics/img-1fb6f67c/posters-game-of-thrones.jpg?H4sIAAAAAAAAA3VOW27EIAy8DpGS2BAIkBvs394gIjy2afNAQNtVT1_Sqp-VZXs80ngG3o9sggfrj-IT7Ktzm4ewbvXKU_J5_fJkUGOLzVTZjSBWdH74ZNMZSUcH1VLRCsbaUYpm-jRVuJv0Rl5KiXkCyEMf12f9VpfNYPcMDKkEVCC00lpYzh1aN8cuF3M4k1zH8TliH49Hi1c1f0EkYiuvACWtO6mJzupVyGt8NPCP3y-GqoL7DSgCsp-JwNnFzvcbRWS1kbNZuEELHox0izdBeoUYLBVej9qZEMIsFmmWRY7Kec_n_hu83S9APwEAAA==",
-    watched: false,
-    score: 0,
-    emmies: 164,
-  },
-  {
-    id: 3,
-    name: "Mad Men",
-    creator: "Matthew Weiner",
-    year: 2007,
-    poster:
-      "https://pics.filmaffinity.com/Mad_Men_Serie_de_TV-351490728-large.jpg",
-    watched: false,
-    score: 0,
-    emmies: 116,
-  },
-  {
-    id: 4,
-    name: "6 feet under",
-    creator: "Alan Ball",
-    year: 2001,
-    poster: "https://www.cine.com/media/series/2711.jpg",
-    watched: false,
-    score: 0,
-    emmies: 53,
-  },
-  {
-    id: 5,
-    name: "Atypical",
-    creator: "Robia Rashid",
-    year: 2017,
-    poster: "https://www.cine.com/media/series/2711.jpg",
-    watched: false,
-    score: 0,
-    emmies: 0,
-  },
+  new Show(
+    1,
+    'The sopranos',
+    'David Chase',
+    1999,
+    'https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
+    21
+  ),
+  new Show(
+    2,
+    'Game of Thrones',
+    'David Benioff D. B. Weiss',
+    2011,
+    'https://t1.pixers.pics/img-1fb6f67c/posters-game-of-thrones.jpg?H4sIAAAAAAAAA3VOW27EIAy8DpGS2BAIkBvs394gIjy2afNAQNtVT1_Sqp-VZXs80ngG3o9sggfrj-IT7Ktzm4ewbvXKU_J5_fJkUGOLzVTZjSBWdH74ZNMZSUcH1VLRCsbaUYpm-jRVuJv0Rl5KiXkCyEMf12f9VpfNYPcMDKkEVCC00lpYzh1aN8cuF3M4k1zH8TliH49Hi1c1f0EkYiuvACWtO6mJzupVyGt8NPCP3y-GqoL7DSgCsp-JwNnFzvcbRWS1kbNZuEELHox0izdBeoUYLBVej9qZEMIsFmmWRY7Kec_n_hu83S9APwEAAA==',
+    164
+  ),
+  new Show(
+    3,
+    'Mad Men',
+    'Matthew Weiner',
+    2007,
+    'https://pics.filmaffinity.com/Mad_Men_Serie_de_TV-351490728-large.jpg',
+    116
+  ),
+  new Show(
+    4,
+    '6 feet under',
+    'Alan Ball',
+    2001,
+    'https://www.cine.com/media/series/2711.jpg',
+    53
+  ),
+  new Show(
+    5,
+    'Atypical',
+    'Robia Rashid',
+    2017,
+    'https://www.cine.com/media/series/2711.jpg',
+    0
+  ),
 ];
